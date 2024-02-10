19d63454a848f45008e24036edc59fc64c951776
Chen6528 <Jamespichen2005@gmail.com>
2024-02-10 10:17:48 -0500

    made movement

--- Player.py
+++ Player.py
@@ -1,4 +1,20 @@
+import pygame
 
 class Player:
     def __init__(self) -> None:
         pass
+
+    def movement():
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        print("Moved Left")
+                    if event.key == pygame.K_RIGHT:
+                        print("Moved Right")
+                    if event.key == pygame.K_DOWN:
+                        print("Moved down")
+                    if event.key == pygame.K_UP:
+                        print("Moved up")
+
+        
+    
