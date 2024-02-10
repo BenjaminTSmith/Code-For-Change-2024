2e1629d43c3ca7e5e0e0dd86e522076dca9d17b4
Ben <smithbenjamin@ufl.edu>
2024-02-10 09:05:51 -0500

    pygame loop

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-import time
+import pygame
 
 
+pygame.init()
+
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event == pygame.QUIT:
+            running = False
+
+    screen.fill("black");
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
