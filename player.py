7657c490a9c406d3b85c7d1732f44f144493e012
Ben <smithbenjamin@ufl.edu>
2024-02-10 13:36:52 -0500

    added player physics

--- player.py
+++ player.py
@@ -6,48 +6,27 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill("white")
         self.rect = self.image.get_rect(topleft=pos)
-        self.move_x = 0 
-        self.move_y = 0 
-        self.accer = 0
-        self.brake = 0
-        self.vel = 5
+        self.x_speed = 0 
+        self.y_speed = 0 
+        self.gravity = 0.08
 
     def movement(self): #make accerlation and brake
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
-            self.move_x = self.vel
+            self.x_speed = 5
         elif keys[pygame.K_LEFT]:
-            self.move_x = -self.vel
+            self.x_speed = -5
         else:
-            self.move_x = 0
+            self.x_speed = 0
 
-            '''if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.move_x = -self.vel
-                    print("moved left: ", self.move_x)
-                if event.key == pygame.K_RIGHT:
-                    self.move_x = self.vel
-                    print("moved right: ",self.move_x)
-                if event.key == pygame.K_DOWN:
-                    self.move_y = self.vel
-                    print("moved down: ", self.move_y)
-                if event.key == pygame.K_UP:
-                    self.move_y = -self.vel
-                    print("moved up: ", self.move_y)
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT:
-                    self.move_x = 0
-                if event.key == pygame.K_RIGHT:
-                    self.move_x = 0
-                if event.key == pygame.K_DOWN:
-                    self.move_y = 0
-                if event.key == pygame.K_UP:
-                    self.move_y += 0'''
+    def apply_physics(self):
+        self.y_speed += self.gravity
 
     def update(self, *args, **kwargs):
         self.movement()
-        self.rect.x += self.move_x
-        self.rect.y += self.move_y
+        self.apply_physics()
+        self.rect.x += self.x_speed
+        self.rect.y += self.y_speed
 
                     
 
