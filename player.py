f1e974668385ee3492bd6abf20e44ded614d218e
Chen6528 <Jamespichen2005@gmail.com>
2024-02-10 16:54:16 -0500

    made some ground

--- player.py
+++ player.py
@@ -17,7 +17,7 @@ class Player(pygame.sprite.Sprite):
         self.idle = 0
         self.run = 0
         self.chopping = 0
-        self.animation_speed = 0.1
+        self.animation_speed = 0.15
 
         # movement settings
         self.x_speed = 0 
