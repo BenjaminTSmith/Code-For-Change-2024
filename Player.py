9ce8d02d53298f6d6452575dd79775535b1d634e
Chen6528 <Jamespichen2005@gmail.com>
2024-02-10 10:43:52 -0500

    Co-authored-by: colinmendoza121 <colinmendoza121@users.noreply.github.com>

--- Player.py
+++ Player.py
@@ -1,20 +1,37 @@
 import pygame
 
-class Player:
-    def __init__(self) -> None:
-        pass
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.move_x = 0 
+        self.move_y = 0 
 
-    def movement():
+    #def sprite(self):
+
+    def movement(self, x, y):
+        #create accerlation, velocity, brakes
+        events = pygame.event.get()
         for event in events:
             if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_LEFT:
+                        
+                        move_x -= x
                         print("Moved Left")
                     if event.key == pygame.K_RIGHT:
+                        move_x += x
                         print("Moved Right")
                     if event.key == pygame.K_DOWN:
+                        move_y -= y
                         print("Moved down")
                     if event.key == pygame.K_UP:
+                        move_y += y
                         print("Moved up")
+                
+
+    def update(self):
+        self.rect.x = self.rect.x + self.move.x
+
+
 
         
     
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
+import Player
 
 
 pygame.init()
 print("PyGame initialized")
-
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
@@ -13,8 +13,10 @@ while running:
     for event in pygame.event.get():
         if event == pygame.QUIT:
             running = False
+        #Player.movement()
 
     screen.fill("black");
+    
     pygame.display.flip()
     clock.tick(60)
 
