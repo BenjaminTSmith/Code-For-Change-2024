5b7a4943ef3ce91f5bc30809de0e7014c7f2064e
Ben <smithbenjamin@ufl.edu>
2024-02-10 11:08:38 -0500

    added world display

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, sys
 from Player import Player
 from World import *
 from Tile import Tile
@@ -7,20 +7,21 @@ from Tile import Tile
 pygame.init()
 print("PyGame initialized")
 
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 world = World(world, screen)
+world.create_world()
 
 running = True
 
 while running:
     for event in pygame.event.get():
-        if event == pygame.QUIT:
-            running = False
-
-    screen.fill("black");
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    screen.fill("black")
+    world.draw()
     pygame.display.flip()
     clock.tick(60)
 
-pygame.quit()
